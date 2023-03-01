997ab33efad0a2328a8d7935ab718d0e7ebf1707
congtranc0722g1 <trantancongall@gmail.com>
2023-03-01 16:46:06 +0700

    CongTT create and update

--- mobile-shop/src/app/app.module.ts
+++ mobile-shop/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SupplierModule} from "./supplier/supplier.module";
+import {HeaderComponent} from "./home/header/header.component";
+import {BodyComponent} from "./home/body/body.component";
+import {FooterComponent} from "./home/footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SupplierModule
   ],
   providers: [],
   bootstrap: [AppComponent]
