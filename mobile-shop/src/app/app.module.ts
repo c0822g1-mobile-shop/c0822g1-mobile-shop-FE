3a594c3ad7d342f5810c5fb9c229b6e75eee3625
nguyenphuc1811 <nguyenthienphuc1811@gmail.com>
2023-03-01 09:21:41 +0700

    phuc sua app routing module all

--- mobile-shop/src/app/app.module.ts
+++ mobile-shop/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeModule} from "./home/home.module";
+import {FooterComponent} from "./home/footer/footer.component";
+import {HeaderComponent} from "./home/header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- mobile-shop/src/app/warehouse/warehouse.module.ts
+++ mobile-shop/src/app/warehouse/warehouse.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { WarehouseRoutingModule } from './warehouse-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    WarehouseRoutingModule
   ]
 })
 export class WarehouseModule { }
