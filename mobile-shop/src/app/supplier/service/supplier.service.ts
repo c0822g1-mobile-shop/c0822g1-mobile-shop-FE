a2e9b9031f70b3ea377d4b2652dfcacbbc12f7f1
congtranc0722g1 <trantancongall@gmail.com>
2023-03-01 16:35:21 +0700

    CongTT create and update supplier

---
+++ mobile-shop/src/app/supplier/service/supplier.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Supplier} from "../../entity/supplier";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupplierService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  createSupplier(supplier: Supplier){
+    return this.httpClient.post("http://localhost:8080/supplier/add", supplier);
+  }
+
+  findSupplier(id: number){
+    return this.httpClient.get<Supplier>("http://localhost:8080/supplier/supplier?id=" + id);
+  }
+
+  updateSupplier(supplier: Supplier){
+    return this.httpClient.put("http://localhost:8080/supplier/update", supplier);
+  }
+}
