c40fe23a561e5f5d5935f02ad04afacb81db6f39
congtranc0722g1 <trantancongall@gmail.com>
2023-03-01 16:24:29 +0700

    CongTT create and update supplier

--- mobile-shop/src/app/supplier/supplier.module.ts
+++ mobile-shop/src/app/supplier/supplier.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SupplierCreateComponent} from './component/supplier-create/supplier-create.component';
+import {SupplierUpdateComponent} from './component/supplier-update/supplier-update.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule} from "ngx-toastr";
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SupplierCreateComponent, SupplierUpdateComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    RouterModule
   ]
 })
-export class SupplierModule { }
+export class SupplierModule {
+}
