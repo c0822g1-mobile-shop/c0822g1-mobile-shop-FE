8c131e3f3752f6634e758961d085a2ec85565fed
cuongvoz <mainvov@gmail.com>
2023-03-08 22:25:30 +0700

    CuongVV push code

--- mobile-shop/src/app/log-in/log-in.module.ts
+++ mobile-shop/src/app/log-in/log-in.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { LogInRoutingModule } from './log-in-routing.module';
 import {LoginComponent} from "./login/login/login.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import { SignupComponent } from './login/signup/signup.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule,
     LogInRoutingModule,
